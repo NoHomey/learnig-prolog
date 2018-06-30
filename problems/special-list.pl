483c87dfe1e971a9d8e2838e67c45dc352fde664
NoHomey <ivo.stratev.tues@gmail.com>
2018-06-30 20:05:28 +0300

    fixing base condition for check

--- problems/special-list.pl
+++ problems/special-list.pl
@@ -1,14 +1,14 @@
-% L = [X1, X2, ..., XN]
+% L = [X1, X2, ..., XN] is list of integers.
+% L is special if :
 % forall k : 1 <= K <= N / 2 is true that:
 % X(2K) = X(X(K + 2)), if 1 <= K + 2 <= N and 1 <= X(K + 2) <= N
 % X(2K) = 2XK + 2, else
-% than L is special.
 % p(L) is true if L is special.
 
 element(1, [H|_], H).
 element(K, [_|T], E) :- K > 1, K1 is K - 1, element(K1, T, E).
 
-check(1, 0, []).
+check(1, 0, 1, [_]).
 check(K, M, _, _) :- K is M + 1.
 check(K, M, N, L) :- K2 is K + 2, K2 =< N, element(K2, L, X), 1 =< X, X =< N, element(X, L, Y),
                     T is 2 * K, element(T, L, Z), Z is Y, S is K + 1, check(S, M, N, L).
